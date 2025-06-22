910e92644e015b5521c976448903b0dc486b487e
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2025-06-23 00:44:36 +0530

    minor chnages in urls

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://expenzo-frontend-six.vercel.app/",
     credentials: true,
   })
 );
